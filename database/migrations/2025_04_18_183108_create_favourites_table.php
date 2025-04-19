a4ce82f2c36da19bf1f02510ae2214c64e2128db
SimonMizerak <145459594+SimonMizerak@users.noreply.github.com>
2025-04-19 18:38:10 +0200

    Added UserController

--- database/migrations/2025_04_18_183108_create_favourites_table.php
+++ database/migrations/2025_04_18_183108_create_favourites_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('product_id');
             $table->unsignedBigInteger('user_id');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');ľ
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+
+Route::resource('users', UserController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
